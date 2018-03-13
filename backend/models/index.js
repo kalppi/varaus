c5d557cf260d3d5bdebcde883f85cfe21c66c9f6
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2018-03-13 13:37:26 +0200

    sql trigger

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,11 +6,12 @@ pg.defaults.ssl = true;
 
 const sequelize = new Sequelize(process.env.DB, {
 	operatorsAliases: false,
+	dialect: 'postgres',
 	logging: process.env.NODE_ENV === 'dev' ? console.log : false
 });
 
 const modelFiles = fs.readdirSync(__dirname)
-	.filter(file => !file.startsWith('index'));
+	.filter(file => !file.startsWith('index') && file.endsWith('.js'));
 
 
 const models = {};
@@ -27,5 +28,9 @@ Object.keys(models).forEach(modelName => {
 		models[modelName].associate(models);
 	}
 });
+
+const query = fs.readFileSync(__dirname + '/query.sql');
+
+sequelize.query(query.toString());
 
 export { sequelize, models };
---
+++ backend/models/query.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION is_valid
+	(s DATE, e DATE, OUT result BOOLEAN)
+	AS $$
+BEGIN
+	SELECT COUNT(*) = 0
+	FROM "Bookings" b
+	WHERE
+		(s >= b.start AND s < b.end)
+	 	OR
+	 	(e > b.start AND e <= b.end)
+	 	OR
+	 	(s < b.start AND e > b.end)
+	 INTO result;
+	 RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION validity_check() RETURNS trigger AS $$
+BEGIN
+	IF(SELECT is_valid(NEW.start, NEW."end") IS false) THEN
+		RAISE EXCEPTION 'invalid';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS validity_check ON "Bookings";
+CREATE TRIGGER validity_check BEFORE INSERT OR UPDATE ON "Bookings"
+	FOR EACH ROW EXECUTE PROCEDURE validity_check();
